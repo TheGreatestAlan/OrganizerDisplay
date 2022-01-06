829e9f8efda180726c301f2214c3780784db7524
Alan.Nguyen <alan.hoang.nguyen@gmail.com>
2022-01-05 22:41:20 -0700

    adding API call to organizer server

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,47 @@
 <template>
   <div id="app">
+  <p>{{ hello }}</p>
+  <!--<button @click="hello++">THIS IS A BUTTON</button>
     <goalsHeader />
     <goals 
       :goalItems="goalItems"
-    />
+    />-->
   </div>
 </template>
 
 <script>
-import goalsHeader from './components/goalsHeader.vue'
-import goals from './components/goals.vue'
+//import goalsHeader from './components/goalsHeader.vue'
+//import goals from './components/goals.vue'
+import EventService from '@/services/EventService.js'
 
 export default{
   name: 'app',
   components: {
-    goalsHeader,
-    goals
+    //goalsHeader,
+    //goals
+  },
+  created() {
+    EventService.getEvents()
+      .then(response => {
+        this.hello = response.data
+      })
+      .catch(error => {
+        console.log("uh ohs:" + error)
+      })
   },
   data() {
     return{
-      goalItems: [
+      /*goalItems: [
         {name:'goal1'}, 
         {name:'goal2'}
-      ]
+      ],*/
+      hello:3
     }
   }
 }
 </script>
+
+
 
 <style>
 #app {
